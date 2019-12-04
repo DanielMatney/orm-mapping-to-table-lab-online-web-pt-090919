1a7949147ffc05df7721460e5e42eca3caa78553
Daniel Matney <danieljmatney@gmail.com>
2019-12-04 05:29:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,4 +3,15 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
+  attr_accessor :name, :album
+ 
+  def initialize(name, album)
+    @name = name
+    @album = album
+  end
+  
+  def save
+    
+  end
+  
 end
